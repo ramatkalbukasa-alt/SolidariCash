3e10ca1cfb68cf24659d9ed63930743c05f910a4
ramatkalbukasa-alt <ramatkalbukasa@gmail.com>
2026-05-30 15:00:47 +0300

    feat: auto-create admin account during Render build

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ pip install -r requirements.txt
 echo "==> Running migrations..."
 python manage.py migrate --no-input
 
+echo "==> Creating default admin account..."
+python manage.py create_admin
+
 echo "==> Collecting static files..."
 python manage.py collectstatic --no-input --clear
 
